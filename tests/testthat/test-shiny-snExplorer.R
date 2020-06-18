7e3760a8a555c66855fb81bca2f919ebdfac5a41
Brendan Knapp <brendan.g.knapp@gmail.com>
2020-06-18 10:09:59 -0700

    trying tests on CI

--- tests/testthat/test-shiny-snExplorer.R
+++ tests/testthat/test-shiny-snExplorer.R
@@ -5,7 +5,7 @@ context("app-file")
 
 test_that("snExplorer works", {
     skip_on_cran()
-    skip_on_ci() # something's off on system dependencies
+    # skip_on_ci() # something's off on system dependencies
     
     app_dir <- system.file(package = "snExplorer", "shiny-snExplorer")
     expect_pass(testApp(app_dir, compareImages = FALSE))
