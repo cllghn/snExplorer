0d5e9e195b6ab5569038bed93e5ea6129d454a99
Brendan Knapp <brendan.g.knapp@gmail.com>
2020-06-18 17:55:50 -0700

    run through styler

--- inst/shiny-snExplorer/import_modal.R
+++ inst/shiny-snExplorer/import_modal.R
@@ -1,21 +1,21 @@
 modal_import_data <- function() {
   showModal(
     modalDialog(
-        box(
-            width = 12,
-            tags$style("<center>"),
-            selectInput(
-                inputId = "input_type",
-                label   = tags$h4("Select data importing option"),
-                choices = list(
-                    "",
-                    "Use internal data" = "internal_data",
-                    "Use external data" = "external_data"
-                )
-            ),
-            tags$br(),
-            uiOutput("data_importation")
-        )
+      box(
+        width = 12,
+        tags$style("<center>"),
+        selectInput(
+          inputId = "input_type",
+          label = tags$h4("Select data importing option"),
+          choices = list(
+            "",
+            "Use internal data" = "internal_data",
+            "Use external data" = "external_data"
+          )
+        ),
+        tags$br(),
+        uiOutput("data_importation")
+      )
     )
   )
 }
--- tests/testthat/test-shiny-snExplorer.R
+++ tests/testthat/test-shiny-snExplorer.R
@@ -4,10 +4,9 @@ context("app-file")
 # This file is for testing the applications in the inst/ directory.
 
 test_that("snExplorer works", {
-    skip_on_cran()
-    # skip_on_ci() # something's off on system dependencies
-    
-    app_dir <- system.file(package = "snExplorer", "shiny-snExplorer")
-    expect_pass(testApp(app_dir, compareImages = FALSE))
-    
+  skip_on_cran()
+  # skip_on_ci() # something's off on system dependencies
+
+  app_dir <- system.file(package = "snExplorer", "shiny-snExplorer")
+  expect_pass(testApp(app_dir, compareImages = FALSE))
 })
